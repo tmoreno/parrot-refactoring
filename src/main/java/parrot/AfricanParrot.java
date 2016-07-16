3da6894a463b5611acafc60c8b4cfbaa9b7eb672
tmoreno <tmorenobernal@yahoo.es>
2016-07-16 17:41:50 +0200

    Replace magic number

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -10,6 +10,6 @@ public class AfricanParrot extends Parrot {
 
 	@Override
 	public double getSpeed() {
-		return Math.max(0, BASE_SPEED - LOAD_FACTOR * numberOfCoconuts);
+		return Math.max(MIN_SPEED, BASE_SPEED - LOAD_FACTOR * numberOfCoconuts);
 	}
 }
--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -4,6 +4,7 @@ public abstract class Parrot {
 
 	protected static final double BASE_SPEED = 12.0;
 	protected static final double LOAD_FACTOR = 9.0;
+	protected static final double MIN_SPEED = 0.0;
 
 	public abstract double getSpeed();
 
