4796a8d0c99687dd9d180967d1159e244a7fbae8
tmoreno <tmorenobernal@yahoo.es>
2016-07-16 17:03:42 +0200

    Added european parrot class

---
+++ src/main/java/parrot/EuropeanParrot.java
@@ -0,0 +1,14 @@
+package parrot;
+
+public class EuropeanParrot extends Parrot {
+
+	public EuropeanParrot(ParrotTypeEnum _type, int numberOfCoconuts,
+			double voltage, boolean isNailed) {
+		super(_type, numberOfCoconuts, voltage, isNailed);
+	}
+
+	@Override
+	public double getSpeed() {
+		return 12.0;
+	}
+}
